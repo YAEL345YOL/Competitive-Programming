faf3371c03bff17e2000183fc33fb414b0f5f5e6
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2025-05-14 21:32:02 -0600

    Update and rename Subsets.cpp to subsets.cpp

---
+++ Math/Bitmasking/subsets.cpp
@@ -0,0 +1,13 @@
+void bitmaskSubsets(int n){
+    for(int i=0;i<(1<<n);++i){
+        for(int j=0;j<n;++j){
+            if(i & (1<<j)){
+                // j bit on (included)
+            }
+            else{
+                // j bit off (excluded)
+            }
+        }
+        // logic for subset
+    }
+}
