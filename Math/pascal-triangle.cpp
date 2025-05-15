91bab74245554519f71cb987db8952b1f625fd26
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2025-05-14 23:27:04 -0600

    Update pascal-triangle.cpp

--- Math/pascal-triangle.cpp
+++ Math/pascal-triangle.cpp
@@ -1,24 +1,11 @@
-template <class T>
-struct pascal{
-    vector<vector<T>>matrix;
-    pascal(T n){
-        matrix.resize(n,vector<T>(n,0));
-        matrix[0][0] = 1;
-        for(T i=1;i<n;++i){
-            for(T j=0;j<n;++j){
-                matrix[i][j] = matrix[i-1][j] + (j > 0 ? matrix[i-1][j-1] : 0);
-            }
+vector<vector<ll>>matrix;
+
+void pascal(){
+    matrix.resize(N,vector<ll>(N,0));
+    matrix[0][0] = 1;
+    for(ll i=1;i<matrix.size();++i){
+        for(ll j=0;j<=i;++j){
+            matrix[i][j] = matrix[i-1][j] + (j > 0 ? matrix[i-1][j-1] : 0);
         }
     }
-    void print(){
-        for(int i=0;i<matrix.size();++i){
-            for(int j=0;j<=i;++j){
-                cout<<matrix[i][j]<<" ";
-            }
-            cout<<"\n";
-        }
-    } 
-    vector<T>& operator[](T index){
-        return matrix[index];
-    }
-};
+}
