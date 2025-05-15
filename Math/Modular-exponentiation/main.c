8f819afab3819df0d6c5347031deaf10f7d65ffe
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2025-05-14 21:33:28 -0600

    Update main.c

--- Math/Modular-exponentiation/main.c
+++ Math/Modular-exponentiation/main.c
@@ -1,16 +1,9 @@
-#include<stdio.h>
-#include<math.h>
-
-#define ll long long
-
-ll p(ll b, ll e, ll m){
-    if(e == 0) return 1LL;
-    ll v = p(b, e/2, m);
-    ll res = (v * v) % m;
-    if(e % 2 != 0) return (res * (b % m)) % m;
-    return res;
-}
-
-int main(){
-    printf("%lld",p(2, 128, 1000000));
-}
+ll fpow(ll b, ll e, ll m){
+    ll r = 1;
+    b %= m;
+    for(;e>0;e>>=1){
+        if(e & 1) r = (r * b) % m;
+        b = (b * b) % m;
+    }
+    return r;
+} 
