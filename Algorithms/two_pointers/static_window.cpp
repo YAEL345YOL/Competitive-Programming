6b14f254fc540d2c3212b4251cfae381fd98d486
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2025-05-31 18:41:43 -0600

    Create static_window.cpp

---
+++ Algorithms/two_pointers/static_window.cpp
@@ -0,0 +1,20 @@
+ll l = 0, r = 0, k;
+
+// starting window size k
+while(r < k){
+    // add a[r]
+
+    ++r;
+}
+
+// Process window [0, k - 1]
+
+while(r < n){
+    // delete a[l]
+    // add a[r]
+
+    // Process window [l, r - 1]
+
+    ++l;
+    ++r;
+}
