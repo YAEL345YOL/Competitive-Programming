71bc995b31c9886f0f149c6d23708a1f7f0c25c5
YAEL345YOL <50465405+YAEL345YOL@users.noreply.github.com>
2025-06-02 21:23:04 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fore(i,in,f) for(auto i=in;i<f;++i)
+#define pb push_back
+#define sp(x) fixed<<setprecision(x)
+#define all(x) x.begin(), x.end()
+#define nl "\n"
+#define ll long long
+#define ul unsigned long long
+
+const int INF = 1e9;
+const ll LL_INF = 1e18;
+const int MOD = 1e9 + 7;
+
+void solve(){}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    
+
+    return 0;
+} 
